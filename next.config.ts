7f65f12fb823e683fc2defed31fc471739f3d14e
JasonEvan <7ason3van@gmail.com>
2026-07-18 09:36:36 +0700

    fix: resolve hostname supabase config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "gcviglqdisttvcvntmwu.supabase.co",
+        hostname: "*.supabase.co",
       },
     ],
   },
